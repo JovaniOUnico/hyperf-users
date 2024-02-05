530a9761eb4ac3334174b591134f8c652ab145de
JovaniOUnico <jovani.souza.almeida@gmail.com>
2024-02-04 21:11:44 -0300

    fix

--- webserver/app/Middleware/AuthMiddleware.php
+++ webserver/app/Middleware/AuthMiddleware.php
@@ -15,49 +15,46 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use function Hyperf\Support\env;
 
-class AuthMiddleware implements MiddlewareInterface
-{
-      /**
-     * @var ContainerInterface
-     */
-    protected $container;
+class AuthMiddleware implements MiddlewareInterface {
+  /**
+   * @var ContainerInterface
+   */
+  protected $container;
 
-    /**
-     * @var RequestInterface
-     */
-    protected $request;
+  /**
+   * @var RequestInterface
+   */
+  protected $request;
 
-    /**
-     * @var HttpResponse
-     */
-    protected $response;
-    protected $jwtSecretKey;
+  /**
+   * @var HttpResponse
+   */
+  protected $response;
+  protected $jwtSecretKey;
 
-    public function __construct(ContainerInterface $container, HttpResponse $response, RequestInterface $request)
-    {
-        $this->container = $container;
-        $this->response = $response;
-        $this->request = $request;
-        $this->jwtSecretKey = env('JWT_SECRET_KEY');
-    }
+  public function __construct(ContainerInterface $container, HttpResponse $response, RequestInterface $request) {
+    $this->container = $container;
+    $this->response = $response;
+    $this->request = $request;
+    $this->jwtSecretKey = env('JWT_SECRET_KEY');
+  }
 
-    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
-    {
-        $token = $this->request->getHeader('Authorization');
+  public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
+    $token = $this->request->getHeader('Authorization');
 
-        if (empty($token)) {
-            return $this->response->json(['error' => 'Token de autenticação ausente'], 401);
-        }
-        
-        try {
-            
-            $decoded = JWT::decode($token[0], new Key($this->jwtSecretKey, 'HS256'));
-            $this->container->set('user', $decoded);
-        } catch (\Exception $e) {
-            return $this->response->json(['error' => 'Token de autenticação inválido'], 401);
-        }
-        
-        return $handler->handle($request);
+    if (empty($token)) {
+      return $this->response->json(['error' => 'Token de autenticação ausente'], 401);
     }
     
+    try {
+      
+      $decoded = JWT::decode($token[0], new Key($this->jwtSecretKey, 'HS256'));
+      $this->container->set('user', $decoded);
+    } catch (\Exception $e) {
+      return $this->response->json(['error' => 'Token de autenticação inválido'], 401);
+    }
+    
+    return $handler->handle($request);
+  }
+  
 }
